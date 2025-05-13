f34d51b0b45783ea0b56560e283441e21a004bcd
Verokeli <ssheng11@my.bcit.ca>
2025-05-12 22:27:32 -0700

    Fixed the redirect issues for unauthorized users to login

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,16 @@
 function requireAuth(req, res, next) {
   if (!req.session || !req.session.name) {
-    return res.redirect("/");
+    return res.redirect("/login");
   }
   next();
 }
 
 function requireAdmin(req, res, next) {
-  if (req.session.user_type !== "admin") {
-    return res.status(403).render("403");
+  if (!req.session.userId) {
+    return res.redirect('/login');
+  }
+  if (req.session.userType !== 'admin') {
+    return res.status(403).send('Forbidden: Admins only');
   }
   next();
 }
